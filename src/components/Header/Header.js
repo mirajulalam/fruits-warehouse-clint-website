03da1b67995181c1095a17389cd6e3ab56e008cf
mirajul alam <alifmirajulalam@gmail.comm>
2022-04-29 14:27:42 +0600

    components and header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="white" variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to="/" className='text-black fw-bold fs-3'>Fruits Warehouse</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                        </Nav>
+                        <Nav>
+                            <Nav.Link as={Link} to='/home'> <button type="button" class="btn btn-link text-decoration-none text-black fw-bold">Home</button></Nav.Link>
+                            <Nav.Link href="#deets"> <button type="button" class="btn btn-link text-decoration-none text-black fw-bold ">Blogs</button></Nav.Link>
+                            <Nav.Link href="#memes">
+                                <button type="button" class="btn btn-link text-decoration-none text-black fw-bold ">Log In</button>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
